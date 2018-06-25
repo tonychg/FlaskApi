b3d2efae290053636f1e1a6fa12e08d46f5692f8
CHINY Antoine <antoine.chiny@inria.fr>
2018-06-25 14:32:34 +0200

    Add main.py + update gitignore
    Signed-off-by: CHINY Antoine <antoine.chiny@inria.fr>

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+    Author      : github.com/TonyChG
+    Date        : lun. 25 juin 2018 14:28:46 CEST
+    Description : 
+    Usage       :
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
