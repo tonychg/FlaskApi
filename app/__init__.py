e19dd1b2cb610b6ad8d39251eaa49fe317fe1b98
CHINY Antoine <antoine.chiny@inria.fr>
2018-06-28 12:38:24 +0200

    Add requirement.txt
    Signed-off-by: CHINY Antoine <antoine.chiny@inria.fr>

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+    Author      : github.com/TonyChG
+    Date        : mer. 27 juin 2018 15:33:24 CEST
+    Description : 
+    Usage       :
+
+"""
+
+
